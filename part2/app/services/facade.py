97d219b807d2b7877ffb994df831d69d65f57512
Sijin35 <sijinsingh222@gmail.com>
2026-06-14 10:29:52 +0200

    Set up methods for amenity, place and review classes with placholder code blocks

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -10,7 +10,7 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
+    # User related methods
     def create_user(self, user_data):
         user = User(**user_data)
         self.user_repo.add(user)
@@ -22,7 +22,61 @@ class HBnBFacade:
     def get_user_by_email(self, email):
         return self.user_repo.get_by _attribute('email', email)
 
-    # Placeholder method for fetching a place by ID
+    # Amenities related methods
+    def create_amenity(self, amenity_date):
+        # Placeholder
+        pass
+
+    def get_amenity(self, amenity_id):
+        # Placeholder
+        pass
+
+    def get_all_amenities(self):
+        # Placeholder
+        pass
+
+    def update_amenity(self, amenity_id, amenity_data):
+        # Placeholder
+        pass
+
+    # Place realted methods
+    def create_place(self, place_data):
+        # Placeholder
+        pass
+
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
+        pass
+
+    def get_all_places(self):
+        # Placeholder
+        pass
+
+    def update_place(self, place_id, place_data):
+        # Placeholder
+        pass
+
+    # Review realated methods
+    def create_review(self, review_data):
+        # Placeholder
+        pass
+
+    def get_review(self, review_id):
+        # Placeholder
+        pass
+
+    def get_all_reviews(self):
+        # Placeholder
+        pass
+
+    def get_reviews_by_place(self, place_id):
+        # Placeholder
+        pass
+
+    def update_review(self, review_id, review_data):
+        # Placeholder
+        pass
+
+    def delete_review(self, review_id):
+        # Placeholder
         pass

0c8d5fdc55b24b995c560b48d1661fb0e460a992
Sijin35 <sijinsingh222@gmail.com>
2026-06-15 12:30:39 +0200

    Made user optional for testing purposes

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,12 +1,12 @@
  #!/usr/bin/python3
 
 
-from base_model import BaseModel
+from app.models.base_model import BaseModel
 
 
 class User(BaseModel):
 
-    def __init__(self, first_name, last_name, email, is_admin):
+    def __init__(self, first_name, last_name, email, is_admin=False):
         super().__init__()
         self.first_name = first_name
         self.last_name = last_name

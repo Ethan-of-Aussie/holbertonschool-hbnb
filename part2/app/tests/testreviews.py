f5780a559d285e06e912ad589bfc2ebbbf39d70e
Sijin35 <sijinsingh222@gmail.com>
2026-06-20 10:34:04 +0000

    Framework for tests (WIP)

---
+++ part2/app/tests/testreviews.py
@@ -0,0 +1,16 @@
+#!/usr/bin.python3
+import unittest
+from app import create_app
+"""Testing review endpoint"""
+
+
+class TestReviewEndpoints(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+    def test_create_review(self):
+        response = self.client.post('api/v1/reviews/', json={
+            })
+        self.assertEqual(response.status_code, 201)

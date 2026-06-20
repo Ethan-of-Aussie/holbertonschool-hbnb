d95270027a2140a39a24a7b15231f7cb6bf110c1
Sijin35 <sijinsingh222@gmail.com>
2026-06-20 10:38:52 +0000

    Test pattern for place (WIP)

---
+++ part2/app/tests/testplace.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Testing place endpoint"""
+import unittest
+from app import create_app
+
+
+class TestPlaceEndpoints(unittest.TestCase):
+
+    def setUP(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+    def test_create_place(self):
+        response = self.client.post('/api/v1/place/', json={
+            })
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_place_invalid_data(self):
+        response = self.client.post('/api/v1/place/', json={
+            })
+        self.assertEqual(response.status_code, 400)
+
+    if __name__ == '__main__':
+        unittest.main(verbosity=2)
